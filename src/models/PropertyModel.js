a55db532cf92e1bd0c98c37e7998ae77a433ae54
Janeil Tan <janeil.tan@developersinstitute.co.nz>
2023-05-24 15:54:29 +1200

    Schema created

--- src/models/PropertyModel.js
+++ src/models/PropertyModel.js
@@ -1,1 +1,28 @@
 // FIXME add your Mongoose schema and model here
+const mongoose = require("mongoose");
+const  { Schema } = mongoose;
+
+const propertySchema = new Schema(
+  {
+    // productId: { type: mongoose.ObjectId, required: true },
+    description: { type: String, required: true },
+    address: { type: String, required: true },
+    title: { type: String, required: true },
+    img: { type: String, required: true },
+    askingPrice: { type: String, required: true },
+
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
+  
+  const Property = mongoose.model("Property", propertySchema); 
+
+  module.exports = Property;
